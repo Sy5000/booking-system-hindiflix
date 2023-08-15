e884881b77d176cf15bda97d10393aee253fde23
Sy5000 <sy.d@hotmail.co.uk>
2023-08-15 23:04:21 +1000

    edit comments in/out

--- views/ajaxCreateBooking.php
+++ views/ajaxCreateBooking.php
@@ -4,33 +4,33 @@ session_start();
 require('../model.php');
 
 // return a new patron required for new booking
-//   $patronID = add_patron($_SESSION['firstName'], $_SESSION['lastName'], $_SESSION['email'], $_SESSION['mobile']);
+  $patronID = add_patron($_SESSION['firstName'], $_SESSION['lastName'], $_SESSION['email'], $_SESSION['mobile']);
   
-//   if($patronID){
+  if($patronID){
 
-//        // calculate # of booking records
-//        $reservations = $_SESSION['adultTickets'] + $_SESSION['childTickets'];
+       // calculate # of booking records
+       $reservations = $_SESSION['adultTickets'] + $_SESSION['childTickets'];
        
-//        echo "Thank you your booking reference is : S-", $_SESSION['sessionID'], " R-"; 
+       echo "Thank you your booking reference is : S-", $_SESSION['sessionID'], " R-"; 
     
-//         for($i=1; $i <= $reservations; $i++){
+        for($i=1; $i <= $reservations; $i++){
     
-//           // record the bookings, output 
-//           $bookingID = addBooking($patronID, $_SESSION['sessionID']);
+          // record the bookings, output 
+          $bookingID = addBooking($patronID, $_SESSION['sessionID']);
           
-//           echo $bookingID;
+          echo $bookingID;
           
-//           // sperate each tix 
-//           if($i != $reservations){
-//             echo "-";
-//           }
+          // sperate each tix 
+          if($i != $reservations){
+            echo "-";
+          }
           
-//         }
+        }
     
-//        echo ".";
+       echo ".";
     
-//       } else {
-//         echo "Could not create booking!";
-//       }
+      } else {
+        echo "Could not create booking!";
+      }
 
 ?> 
